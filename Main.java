76372a0752fa9f8b946b342c550a4f6058139bc2
Myra Deister <mjdeister@fjuhsd.org>
2021-01-13 17:30:17 +0000

    updated Main

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ class Main
   /* Main method for testing */
   public static void main(String[] args) 
   {
-    Picture pic = new Picture("caterpillar.jpg");
-     
+    Picture pic = new Picture("beach.jpg");
+     pic.decreaseBlue(.60);
     pic.write("output.jpg");
   }
 }
