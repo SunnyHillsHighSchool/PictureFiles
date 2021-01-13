e3dd60ca950dd5b2fcddbceca0daad639445d711
Myra Deister <mjdeister@fjuhsd.org>
2021-01-13 17:17:49 +0000

    added decreaseBlue method

--- Picture.java
+++ Picture.java
@@ -367,6 +367,23 @@ public class Picture
   }
 
    ////////////////////// methods ///////////////////////////////////////
+  //precondition: amount is between 0 and 1
+public void decreaseBlue(double amount)
+   {
+   //create a pixel array
+   Pixel[]pixelArray=this.getPixels();
+   //create a pixel object
+   Pixel p  = null;
+   //loop through all of the pixels
+   for(int x=0;x<pixelArray.length;x++)
+   {
+   //get the current pixel
+   p =pixelArray[x];
+    //decrease the blue value
+    p.setBlue((int)(p.getBlue()*amount));
+   }
+   }
+   
 /*
 The copper-puzzle.png image is a puzzle -- it shows something famous, however the image has been distored. The true image is in the blue and green values, however all the blue and green values have all be divided by 20, so the values are very small. The red values are all just random numbers, noise added on top to obscure things. Undo these distortions to reveal the true image.
 
